1c65ddf8854c520e29d0efbe326f0e28a5895434
Lucas Reiter <lucas.freiter@gmail.com>
2021-09-27 21:25:47 -0300

    Adição dos métodos de controle - Aula-04-05

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,6 +1,15 @@
+import { Negociacoes } from './negociacoes';
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao> {
+        return this.negociacoes;
+    }
 
 }
